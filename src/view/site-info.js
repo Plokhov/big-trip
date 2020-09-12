42d586ae4812214273fe9e134d00cd591e18ca09
Mikhail <plohov.mm@mail.ru>
2020-09-12 14:18:58 +0500

    Личный проект. Раздел 4. Революция или эволюция? (Часть 2)

--- src/view/site-info.js
+++ src/view/site-info.js
@@ -7,34 +7,44 @@ export default class SiteInfo {
   }
 
   getTemplate() {
-    const {
-      cities,
-      dateStart,
-      dateFinish,
-      totalCost,
-    } = this._itinerary;
+    const {totalCost = 0} = this._itinerary;
 
-    const infoTitleTemplate = cities.length > 3
-      ? `${cities[0]}  &mdash; ...  &mdash; ${cities[cities.length - 1]}`
-      : `${cities.join(`&nbsp;&mdash;&nbsp;`)}`;
+    if (Object.keys(this._itinerary).length !== 0) {
+      const {
+        cities,
+        dateStart,
+        dateFinish,
+      } = this._itinerary;
 
-    return (
-      `<section class="trip-main__trip-info  trip-info">
-        <div class="trip-info__main">
-          <h1 class="trip-info__title">${infoTitleTemplate}</h1>
+      const infoTitleTemplate = cities.length > 3
+        ? `${cities[0]}  &mdash; ...  &mdash; ${cities[cities.length - 1]}`
+        : `${cities.join(`&nbsp;&mdash;&nbsp;`)}`;
 
-          <p class="trip-info__dates">
-            ${createShortDateTemplate(dateStart)}
-            &nbsp;&mdash;&nbsp;
-            ${createShortDateTemplate(dateFinish)}
+      return (
+        `<section class="trip-main__trip-info  trip-info">
+          <div class="trip-info__main">
+            <h1 class="trip-info__title">${infoTitleTemplate}</h1>
+            <p class="trip-info__dates">
+              ${createShortDateTemplate(dateStart)}
+              &nbsp;&mdash;&nbsp;
+              ${createShortDateTemplate(dateFinish)}
+            </p>
+          </div>
+
+          <p class="trip-info__cost">
+            Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalCost}</span>
           </p>
-        </div>
-
-        <p class="trip-info__cost">
-          Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalCost}</span>
-        </p>
-      </section>`
-    );
+        </section>`
+      );
+    } else {
+      return (
+        `<section class="trip-main__trip-info  trip-info">
+          <p class="trip-info__cost">
+            Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalCost}</span>
+          </p>
+        </section>`
+      );
+    }
   }
 
   getElement() {
